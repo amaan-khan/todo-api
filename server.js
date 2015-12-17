b41a42c54e52b871a95abb03128229013be9c0e0
Mohammed Amanullah <mamanull@cisco.com>
2015-12-17 20:28:07 +0530

    added underscore feature

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -16,21 +17,22 @@ app.get('/todos', function (request,response) {
 });
 
 app.post('/todos' , function (request, response) {
-	var body = request.body;
-	body.id = todoNext++;
-	todos.push(body);
+	var body = _.pick(request.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return response.status(400).send();
+	}
+
+	body.description = body.description.trim();
+	body.id = todoNext++;					// add id field
+	todos.push(body);						// push to array
 	console.log('description '  + body.description);
 	response.json(body);
 })
 
 app.get('/todos/:id', function (request,response) {
 	var todoId = parseInt(request.params.id, 10);
-	var matchedtodo;
-	todos.forEach(function (todo) {
-		if(todoId === todo.id){
-			matchedtodo=todo;
-		}
-	});
+	var matchedtodo = _.findWhere(todos, {id:todoId});			// find by id, id as object
 	if(matchedtodo){
 		response.json(matchedtodo);
 	}else{
